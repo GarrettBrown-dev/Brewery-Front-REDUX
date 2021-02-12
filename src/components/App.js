ac9b6e5b74b916560e3a42a1687614c9c7374df7
Garrett <gman9mm@live.com>
2021-02-12 08:57:09 -0800

    Create components and add app contents.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Beer from './Beer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Beer />
+      <BeerControl />
+    </>
+  );
+}
+
+export default App;
