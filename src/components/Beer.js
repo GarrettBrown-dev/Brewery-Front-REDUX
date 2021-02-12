e470f20ba58bed8aed11ed9e20bf07d684b1aab9
Garrett <gman9mm@live.com>
2021-02-12 09:45:33 -0800

    Add Beer.js contents.

---
+++ src/components/Beer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Beer(props) {
+  return (
+    <>
+      <div onClick={() => props.whenBeerClicked(props.id)}>
+        <p>props.name</p>
+      </div>
+    </>
+  );
+}
+
+Beer.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  alcoholContent: PropTypes.string,
+  price: PropTypes.string,
+  quantity: PropTypes.string,
+  id: PropTypes.string,
+};
+
+export default Beer;
