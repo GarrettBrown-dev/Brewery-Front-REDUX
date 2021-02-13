f36334c751d807ef5ef9aa8e30c0e221d6bd90fa
Garrett <gman9mm@live.com>
2021-02-12 16:03:54 -0800

    Add background... which I'm not the biggest fan of honestlly.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Icon from './../img/Icon.png';
 
 function Header() {
   return (
     <React.Fragment>
       <h1 align="center">Brewski Brew Lodge</h1>
-      <img align="center" src={Icon} alt="A icon of a lodge" />
     </React.Fragment>
   );
 }
