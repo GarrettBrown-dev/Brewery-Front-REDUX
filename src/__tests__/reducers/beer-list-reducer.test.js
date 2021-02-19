d42388e8afc85e2a33b732e3a5da2ccac913cfc6
Garrett <gman9mm@live.com>
2021-02-19 14:56:22 -0800

    Good fail on test 4.

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -34,4 +34,28 @@ describe('beerListReducer', () => {
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(beerListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new beer to masterBeerList', () => {
+    const { name, brand, alcoholContent, price, quantity, id } = beerInfo;
+    action = {
+      type: 'ADD_BEER',
+      name: name,
+      brand: brand,
+      alcoholContent: alcoholContent,
+      price: price,
+      quantiy: quantity,
+      id: id,
+    };
+
+    expect(beerListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        alcoholContent: alcoholContent,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
 })
